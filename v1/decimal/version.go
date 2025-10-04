1a8f0080b5528ae3985fdd7f521c73cce7703904
Knuriel <estbsn@outlook.com>
2025-10-04 20:10:35 +0800

    Add version.go for decimal
    version.go is a version description file, also records referencing and syncing status

---
+++ v1/decimal/version.go
@@ -0,0 +1,18 @@
+// Project: xma.v1.decimal
+// File: version.go
+
+package decimal
+
+// Acknowledgement:
+//   This implementation is a reference
+//   Referenced Project Name: shopspring/decimal
+//   Referenced Project Owner(s): Jason Biegel (from New York City, US), Mateusz Wos (from Cracow, Poland)
+//   Referenced Project URL: https://github.com/shopspring/decimal
+//   Referencer: x1-tech.com
+//   Reference Project: xma
+//   Version Number Relation pseudo-code: xma.v1.decimal.v1.0.10000 => shopspring.decimal.v1.4.0
+//   Feel grateful to the contributors
+const (
+	Version = "1.0.10000"
+	LastSyncDate = "2025-10-04Z"
+)
