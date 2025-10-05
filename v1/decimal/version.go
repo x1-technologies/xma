86f96bb053b8db81011deeb8d5333e6f41fb5999
Knuriel <estbsn@outlook.com>
2025-10-05 15:45:18 +0800

    Update version.go
    Updated code sync records description

--- v1/decimal/version.go
+++ v1/decimal/version.go
@@ -1,4 +1,4 @@
-// Project: xma.v1.decimal
+// Package: xma/v1/decimal
 // File: version.go
 
 package decimal
@@ -10,8 +10,19 @@ package decimal
 //   Referenced Project URL: https://github.com/shopspring/decimal
 //   Referencer: x1-tech.com
 //   Reference Project: xma
-//   Version Number Relation pseudo-code: xma.v1.decimal.v1.0.10000 => shopspring.decimal.v1.4.0
+//   Version Number Relation pseudo-code:
+//     const (
+//       SyncModeFull = "FULL"
+//       SyncModeBranchMerge = "BRANCH_MERGE"
+//     )
+//     [0]xma.CodeSyncRecord = {
+//       XmaVersion: "1.0.10000",
+//       ShopSpringDecimalVersion: "1.4.0",
+//       SyncMode: SyncModeFull,
+//       SyncDate: "2025-10-04Z",
+//     }
 //   Feel grateful to the contributors
+
 const (
 	Version = "1.0.10000"
 	LastSyncDate = "2025-10-04Z"
